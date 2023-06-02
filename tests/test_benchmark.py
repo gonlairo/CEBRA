1ed527256427e040867cc920a6b375c677ec0412
Rodrigo <gonlairo@gmail.com>
2023-06-02 16:11:28 +0200

    adding hippocampus analysis -> needs consistency testing

--- tests/test_benchmark.py
+++ tests/test_benchmark.py
@@ -35,15 +35,12 @@ loader_params = {
 }
 lr = 3e-4
 test_ratio = 0.2
-lower_bound_score = {"r2_score": 30, "median_error": 1.5}
 
 single_session_setup = {
-    "data_name": "rat-hippocampus-single-achilles",
     "dataset_initfunc": cebra.data.TensorDataset,
     "loader_initfunc": cebra.data.ContinuousDataLoader,
     "solver_initfunc": cebra.solver.SingleSessionSolver,
 }
-
 
 def _split_data(data, test_ratio=0.2):
     split_idx = int(len(data) * (1 - test_ratio))
@@ -109,30 +106,88 @@ def _train(train_set, loader_initfunc, solver_initfunc):
     return solver
 
 
-def _run(data_name, dataset_initfunc, loader_initfunc, solver_initfunc):
-    dataset = cebra.datasets.init(data_name)
-    neural_train, neural_test, label_train, label_test = _split_data(
-        dataset, test_ratio)
-    offset = cebra.data.datatypes.Offset(loader_params["time_offset"] // 2,
-                                         loader_params["time_offset"] // 2)
-    train_set = dataset_initfunc(neural_train,
-                                 continuous=label_train,
-                                 offset=offset)
-    valid_set = dataset_initfunc(neural_test,
-                                 continuous=label_test,
-                                 offset=offset)
+def _run_hippocampus(data_name_dict, dataset_initfunc, loader_initfunc, solver_initfunc, lower_bound_score):
 
-    solver = _train(train_set, loader_initfunc, solver_initfunc)
-    r2_score, pos_err = _eval(train_set, valid_set, solver)
+    results = {}
+    embeddings = {}
+    for mouse in data_name_dict.keys():
+        results_mouse = {}
+        dataset = cebra.datasets.init(f'rat-hippocampus-single-{mouse}')
+        neural_train, neural_test, label_train, label_test = _split_data(
+            dataset, test_ratio)
+        offset = cebra.data.datatypes.Offset(loader_params["time_offset"] // 2,
+                                             loader_params["time_offset"] // 2)
+        train_set = dataset_initfunc(neural_train,
+                                     continuous=label_train,
+                                     offset=offset)
+        valid_set = dataset_initfunc(neural_test,
+                                     continuous=label_test,
+                                     offset=offset)
 
-    assert lower_bound_score["r2_score"] < r2_score
-    assert lower_bound_score["median_error"] > pos_err
+        # TRAIN MODEL
+        solver = _train(train_set, loader_initfunc, solver_initfunc)
+
+        # decoding acc
+        r2_score, pos_err = _eval(train_set, valid_set, solver)
+        results_mouse.update({"r2" : r2_score, "pos_err" : pos_err})
+
+        # goodness of fit (loss)
+        goodness_of_fit = solver.history
+        results_mouse.update({"goodness_of_fit" : goodness_of_fit})
+
+        # save the embeddings
+        results_mouse.update({"embedding":solver.model(train_set)})
+
+        embeddings[mouse] = solver.model(train_set)
+        results[mouse] = results_mouse
+
+        assert r2_score > lower_bound_score["r2_score"] 
+        assert pos_err < lower_bound_score["median_error"]
+        assert goodness_of_fit < lower_bound_score["infonce"]
+    
+
+    # TODO: compute consistency across animals!
+    labels = [data_name_dict[mouse].continuous_index[:, 0]
+          for mouse in list(data_name_dict.keys())]
+
+    time_scores, time_pairs, time_subjects = cebra.sklearn.metrics.consistency_score(embeddings=list(embeddings.values()),
+                                                                                 labels=labels,
+                                                                                 dataset_ids=list(
+                                                                                     embeddings.keys()),
+                                                                                 between="datasets")
 
     print(f"{solver_initfunc.__name__}: r2 score = {r2_score:.4f}, "
           f"median abs error = {pos_err:.4f}")
-    return r2_score, pos_err
+    return None
+    # What exactly should I return?
+    #r2_score, pos_err
 
 
 @pytest.mark.benchmark
-def test_single_session_(benchmark):
-    benchmark.pedantic(_run, kwargs=single_session_setup, rounds=1)
+def test_single_session_hippocampus(benchmark):
+
+    ### TEST HIPPOCAMPUS
+    hippocampus_pos = {}
+    hippocampus_pos["achilles"] = cebra.datasets.init('rat-hippocampus-single-achilles')
+    hippocampus_pos["buddy"] = cebra.datasets.init('rat-hippocampus-single-buddy')
+    hippocampus_pos["cicero"] = cebra.datasets.init('rat-hippocampus-single-cicero')
+    hippocampus_pos["gatsby"] = cebra.datasets.init('rat-hippocampus-single-gatsby')
+
+    lower_bound_score = {"r2_score": 30, "median_error": 1.5, "infonce": 6.5}
+
+    # check if pedantic mode is exactly what we want
+
+    single_session_setup_hippocampus = {
+    "data_name_dict":hippocampus_pos,
+    "dataset_initfunc": cebra.data.TensorDataset,
+    "loader_initfunc": cebra.data.ContinuousDataLoader,
+    "solver_initfunc": cebra.solver.SingleSessionSolver,
+    "lower_bound_score" : lower_bound_score,
+}
+    benchmark.pedantic(_run_hippocampus, kwargs = single_session_setup_hippocampus,
+                                         rounds=1)
+    
+
+@pytest.mark.benchmark
+def test_single_session_allen(benchmark):
+    pass
